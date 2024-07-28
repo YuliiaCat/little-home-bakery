075d220ce70f8e41c0f6fca06ab2fd6587317ccf
Yuliia <kotyuliya24@gmail.com>
2024-07-28 14:52:28 -0400

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/little-home-bakery/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
