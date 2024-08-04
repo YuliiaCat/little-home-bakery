8606aac08550785cdfcd2d27436c219ce4b1e3bd
Yuliia <kotyuliya24@gmail.com>
2024-08-04 18:44:07 -0400

    finish

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,10 @@
-const NotFoundPage = () => <h2>Page not Found</h2>;
+const NotFoundPage = () => {
+  return (
+    <section className="container">
+      <h2 className="title">Page not Found</h2>
+      <img src="./img/page-not-found.webp" alt="Page not found" />
+    </section>
+  );
+}
 
 export default NotFoundPage;
